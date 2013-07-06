4cfaa8e5c25198e80bdd52932cb3c0a7dfe5a0d2
Ilari Shafer <mrcaps@gmail.com>
2013-07-06 14:24:55 -0400

    Add more of the patcher

---
+++ README.md
@@ -0,0 +1,9 @@
+#Diff and compilation utils for wikimedia trace
+
+## dirs
+
+### patch-diff/[timestamp-start]-[timestamp-end].json
+a patch valid from timestamp-start to timestamp-end
+
+### patch-full/...
+content that should be copied in at all times (private password dirs)
--- differ.py
+++ differ.py
@@ -2,10 +2,84 @@ import logging as log
 log.basicConfig(level=log.DEBUG)
 import subprocess
 import unittest
+from distutils import dir_util
+import os
+
+class Patcher(object):
+	def __init__(self, repopath, patchpath="patch-full", diffpath="patch-diff"):
+		self.repopath = repopath
+		self.patchpath = patchpath
+		self.diffpath = diffpath
+
+	def patch(self, timestamp):
+		#copy over the tree...
+		try:
+			log.info("Copying patch files")
+			dir_util.copy_tree(self.patchpath, self.repopath)
+		except Exception, e:
+			log.error("Patch error:" + str(e))
+
+	def grep_patch(self, fpath, orig, replace):
+		"""Replace the text _orig_ in file 
+		with path _fpath_ with _replace_"""
+		replaced = None
+		with open(fpath, "r") as fp:
+			contents = fp.read()
+			replaced = contents.replace(orig, replace)
+		if replaced:
+			print "REPLACING!", replaced
+			with open(fpath, "w") as fp:
+				fp.write(replaced)
+
+	def diff_patch(self):
+		"""Patch from 'our' diff format
+
+		see patch-diff for examples.
+		"""
+		for fname in os.listdir(self.diffpath):
+			self.__apply_diff(os.path.join(self.diffpath, fname))
+
+	def __apply_diff(self, fpath):
+		cur = {
+			"file": None,
+			"add": "",
+			"del": ""
+		}
+
+		def run_apply():
+			log.info("Patching %s" % cur["file"])
+
+			self.grep_patch(os.path.join(self.repopath, cur["file"]), 
+				cur["del"], cur["add"])
+			cur["add"] = ""
+			cur["del"] = ""
+			cur["file"] = None
+
+		with open(fpath, "r") as fp:
+			for line in fp:
+				line = line.strip()
+				if line.startswith("#") or len(line) == 0:
+					continue
+				elif line.startswith("+"):
+					if len(cur["add"]) > 0:
+						cur["add"] += "\n"
+					cur["add"] += line[1:]
+				elif line.startswith("-"):
+					if len(cur["del"]) > 0:
+						cur["del"] += "\n"
+					cur["del"] += line[1:]
+				else:
+					#we have a title
+					if cur["file"]:
+						run_apply()
+					cur["file"] = line
+
+			run_apply()
 
 class Differ(object):
 	def __init__(self, repopath="operations-puppet"):
 		self.repopath = repopath
+		self.patcher = Patcher(repopath)
 
 	def checkout(self, revision):
 		"""Check out at the given revision."""
@@ -21,7 +95,6 @@ class Differ(object):
 			"--templatedir=./templates"], cwd=self.repopath)
 		print("compile output")
 		print(out)
-
 	
 class DifferTest(unittest.TestCase):
 	def test_checkout(self):
@@ -34,5 +107,18 @@ class DifferTest(unittest.TestCase):
 		d.checkout("cf985570ff5dcd5170cc8704747785a14364ff71")
 		d.compile()
 
+	def test_patch(self):
+		p = Patcher(repopath="operations-puppet")
+		p.patch(1369159391)
+
+class IncTest(unittest.TestCase):
+	def test_dpatch(self):
+		p = Patcher(repopath="operations-puppet")
+		p.diff_patch()
+
 if __name__ == "__main__":
 	unittest.main()
+
+	#suite = unittest.TestSuite()
+	#suite.addTests(IncTest)
+	#unittest.TextTestRunner().run(suite)
